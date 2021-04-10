fcb56d9774bac6921f5d4cee364b2fc5cdb52e29
Rey Arqueza <reyarqueza@gmail.com>
2021-04-10 15:55:22 -0700

    srcSet improvements

--- jsx/Card.jsx
+++ jsx/Card.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function(props) {
   return (
     <div className="rs-card">
-      <img src={props.src} alt={props.alt} />
+      <img
+        sizes={props.sizes}
+        srcSet={props.srcSet}
+        src={props.src}
+        alt={props.alt}
+      />
       <p>{props.text}</p>
     </div>
   )
--- jsx/Hero.jsx
+++ jsx/Hero.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function(props) {
   return (
     <div className="rs-hero">
-      <img src={props.src} alt={props.alt} />
+      <img
+        sizes={props.sizes}
+        srcSet={props.srcSet}
+        src={props.src}
+        alt={props.alt}
+      />
       <h1>{props.text}</h1>
       {props.children}
     </div>

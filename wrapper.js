5f2f5bdb44bcd552031c11c7dfa472c5a25da646
Rey Arqueza <reyarqueza@gmail.com>
2022-01-31 23:25:34 -0800

    image sizes again for perf

--- wrapper.js
+++ wrapper.js
@@ -6,10 +6,7 @@ export default function (content, preloadedState) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/images/favicon.ico" />
     <link rel="stylesheet" href="/css/styles.min.css" />
-  </head>
-  <body>
-    <main>${content}</main>
-    <script>
+    <script defer>
     // WARNING: See the following for security issues around embedding JSON in HTML:
     // https://redux.js.org/recipes/server-rendering/#security-considerations
     window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
@@ -17,7 +14,10 @@ export default function (content, preloadedState) {
       "\\u003c"
     )}
     </script>
-    <script src="/js/bundle.js"></script>
+    <script defer src="/js/bundle.js"></script>
+  </head>
+  <body>
+    <main>${content}</main>
   </body>
 </html>`;
 }

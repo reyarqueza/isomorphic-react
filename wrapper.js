b2ad53d50766461a69077c951ef34271c2c2ba71
Rey Arqueza <reyarqueza@gmail.com>
2022-02-01 16:12:52 -0800

    removed comment

--- wrapper.js
+++ wrapper.js
@@ -12,8 +12,6 @@ export default function (content, preloadedState) {
     <link rel="icon" href="/images/favicon.ico" />
     <link rel="stylesheet" media="screen" href="/css/styles.min.css" />
     <script defer>
-    // WARNING: See the following for security issues around embedding JSON in HTML:
-    // https://redux.js.org/recipes/server-rendering/#security-considerations
     window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
     </script>
     <script defer src="/js/bundle.js"></script>
